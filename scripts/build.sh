362512e98416ee29695f60060ba23c8f5360cd13
Archana Karpe <43923595+archanaakarpe@users.noreply.github.com>
2023-04-10 12:29:15 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-if [ ! "$(docker ps -a -q -f name=node-app)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=node-app)" ]; then
+if [ ! "$(docker ps -a -q)" ]; then
         echo "cleanup"
         docker kill $(docker ps -q)
         docker rm $(docker ps -a -q)
         docker rmi $(docker images -q)
-    fi
-    	echo "Running Container"
-    	docker build . -t node-app
-   	    docker run -d --name node-app -p 8000:8000 node-app
 fi
+echo "Running Container"
+docker build . -t node-app
+docker run -d --name node-app -p 8000:8000 node-app
